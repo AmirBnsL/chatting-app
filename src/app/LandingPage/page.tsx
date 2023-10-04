f1095617dfd6c9414927c78103d2268ec3f4f677
AmirBnsL <amir71084@gmail.com>
2023-10-04 23:04:16 +0100

    one another commit

--- src/app/LandingPage/page.tsx
+++ src/app/LandingPage/page.tsx
@@ -1,18 +1,17 @@
 import MainBar from "./main";
+import { ReactNode } from "react";
 
-function LandingPage({ children }: { children: React.ReactNode }) {
-  
-  
+interface LandingPageProps {
+  children: ReactNode;
+}
 
-  
+export default function LandingPage({ children }: LandingPageProps) {
   return (
     <>
       {children}
       <div className="grow bg-pale_azure-700 flex items-center justify-around flex-col h-screen">
-        <MainBar></MainBar>
+        <MainBar />
       </div>
     </>
   );
 }
-
-export default LandingPage;
